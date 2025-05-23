cefc1d4687ec5b29eb1c048548e77e9647ee206f
Zishum <you@example.com>
2025-05-23 03:11:04 +0300

    footer-rafactor

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,82 +1,74 @@
+import React, { Fragment } from "react";
 import styles from "./footer.module.css";
-import call from "/call.svg";
-import location from "/location.svg";
-import mail from "/mail.svg";
-import { Fragment } from "react";
+import callIcon from "/call.svg";
+import locationIcon from "/location.svg";
+import mailIcon from "/mail.svg";
 
-const FooterItem = ({ item }) => {
-	const renderContent = (item) => {
-		switch (item.type) {
-			case "tel":
-				return (
-					<a
-						className={styles["footer__text-item"]}
-						href={`tel:${item.text.replace(/[^\d+]/g, "")}`}
-					>
-						{item.text}
-					</a>
-				);
-			case "email":
-				return (
-					<a className={styles["footer__text-item"]} href={`mailto:${item.text}`}>
-						{item.text}
-					</a>
-				);
-			case "text":
-				return (
-					<div className={styles.footer__text}>
-						{item.text.split("\n").map((line, i) => (
-							<Fragment key={i}>
-								{line ? (
-									<>
-										<p className={styles["footer__text-item"]}>{line}</p>
-									</>
-								) : (
-									<br />
-								)}
-							</Fragment>
-						))}
-					</div>
-				);
-
-			default:
-				return <span>{item.text}</span>;
-		}
-	};
-
-	return (
-		<li className={styles["footer__list-item"]}>
-			<img className={styles["footer__list-icon"]} src={item.icon} alt="" />
-			{renderContent(item)}
-		</li>
-	);
-};
-
-const data = [
+const items = [
 	{
 		type: "tel",
-		icon: call,
+		icon: callIcon,
 		text: "+7 (8352) 24-06-30",
 	},
 	{
 		type: "text",
-		icon: location,
-		text: "Адрес: г. Чебоксары, пр-кт Мира, д. 1\n\nЮридический адрес: 428024, г. Чебоксары,пр. Мира, д. 1, литер 7, этаж 2, помещ. 2",
+		icon: locationIcon,
+		text: `Адрес: г. Чебоксары, пр-кт Мира, д. 1
+
+Юридический адрес: 428024, г. Чебоксары, пр. Мира, д. 1, литер 7, этаж 2, помещ. 2`,
 	},
 	{
 		type: "email",
-		icon: mail,
+		icon: mailIcon,
 		text: "info@iic21.ru",
 	},
 ];
+
+const renderers = {
+	tel: (text) => (
+		<a className={styles["footer__text-item"]} href={`tel:${text.replace(/[^\d+]/g, "")}`}>
+			{text}
+		</a>
+	),
+
+	email: (text) => (
+		<a className={styles["footer__text-item"]} href={`mailto:${text}`}>
+			{text}
+		</a>
+	),
+
+	text: (text) => (
+		<div className={styles.footer__text}>
+			{text.split("\n").map((line, idx) =>
+				line ? (
+					<p key={idx} className={styles["footer__text-item"]}>
+						{line}
+					</p>
+				) : (
+					<br key={idx} />
+				)
+			)}
+		</div>
+	),
+};
+
+const FooterItem = ({ type, icon, text }) => {
+	const Renderer = renderers[type] || ((t) => <span>{t}</span>);
+	return (
+		<li className={styles["footer__list-item"]}>
+			<img className={styles["footer__list-icon"]} src={icon} alt="" />
+			{Renderer(text)}
+		</li>
+	);
+};
 
 export default function Footer() {
 	return (
 		<footer className={styles.footer}>
 			<div className={`${styles.footer__container} container`}>
 				<ul className={styles.footer__list}>
-					{data.map((item, idx) => (
-						<FooterItem key={idx} item={item} />
+					{items.map((item, i) => (
+						<FooterItem key={i} {...item} />
 					))}
 				</ul>
 				<div className={styles.footer__copyright}>
