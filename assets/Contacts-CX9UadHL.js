229ab900b300c191add56ccc886ac93874016621
Zishum <you@example.com>
2025-05-26 18:56:50 +0300

    Updates

--- assets/Contacts-CX9UadHL.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-B-w8kseb.js";const s=()=>t.jsx(t.Fragment,{children:t.jsx("h1",{children:"Contacts"})});export{s as default};
--- assets/index-B2eGMat7.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-B-w8kseb.js";const t=()=>r.jsx(r.Fragment,{children:r.jsx("h1",{children:"Career"})});export{t as default};
