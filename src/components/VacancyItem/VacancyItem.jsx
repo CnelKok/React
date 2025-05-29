e075857f1e516a0f85b718cf52ab16baf7c97aa4
Zishum <you@example.com>
2025-05-29 17:37:59 +0300

    vacancy-load-fix

--- src/components/VacancyItem/VacancyItem.jsx
+++ src/components/VacancyItem/VacancyItem.jsx
@@ -1,14 +1,18 @@
 import styles from "./vacancyitem.module.css";
-
+import { lazy, Suspense } from "react";
 import data from "./data";
-import TextElement from "../TextElement";
+import FallBack from "../../pages/FallBack";
+
+const TextElement = lazy(() => import("../TextElement"));
 
 const VacancyItem = ({ id }) => {
 	const item = data[id];
 	return (
 		<>
 			<div className={`${styles["vacancy-item"]} container`}>
-				<TextElement item={item} type={"vacancy"} />
+				<Suspense fallback={<FallBack />}>
+					<TextElement item={item} type={"vacancy"} />
+				</Suspense>
 			</div>
 		</>
 	);
--- src/pages/Vacancy.jsx
+++ src/pages/Vacancy.jsx
@@ -1,17 +1,12 @@
 import { useParams } from "react-router-dom";
-import { Suspense, lazy } from "react";
-import FallBack from "./FallBack";
-
-const VacancyItem = lazy(() => import("../components/VacancyItem"));
+import VacancyItem from "../components/VacancyItem";
 
 const Vacancy = () => {
 	const { vacancy_id } = useParams();
 	return (
 		<>
-			<div className="margin-top">
-				<Suspense fallback={<FallBack />}>
-					<VacancyItem id={vacancy_id} />
-				</Suspense>
+			<div className="margin-top ">
+				<VacancyItem id={vacancy_id} />
 			</div>
 		</>
 	);
