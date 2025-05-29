3f1a7b28a1e3a1feee4ee827e59865e54a60a5fa
Zishum <you@example.com>
2025-05-29 20:00:44 +0300

    sponsors-template

--- src/components/HomeComponents/Sponsors/Sponsors.jsx
+++ src/components/HomeComponents/Sponsors/Sponsors.jsx
@@ -1,5 +1,16 @@
+import styles from "./sponsors.module.css";
+import data from "./data";
+
 const Sponsors = () => {
-	return <></>;
+	return (
+		<>
+			<div className={styles.sponsors}>
+				{data.map((item, i) => (
+					<></>
+				))}
+			</div>
+		</>
+	);
 };
 
 export default Sponsors;
