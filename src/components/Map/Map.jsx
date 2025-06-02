1983473bcb644c1b382ab4286026ef7c676a6f27
Zishum <you@example.com>
2025-06-03 00:43:34 +0300

    fix

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -2,11 +2,10 @@ const Map = () => {
 	return (
 		<iframe
 			src="https://yandex.ru/map-widget/v1/?lang=ru_RU&amp;scroll=true&amp;source=constructor-api&amp;um=constructor%3Ae6654a5470774adb8df65f9ba6729011e4d4aa6cf4f2cfb7923c132b0a22be39"
-			frameborder="0"
-			allowfullscreen="true"
+			allowFullScreen={true}
 			width="100%"
 			height="710px"
-			style={{ display: "block" }}
+			style={{ display: "block", border: 0 }}
 		></iframe>
 	);
 };
