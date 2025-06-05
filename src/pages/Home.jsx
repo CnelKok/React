3f389c6065913ed01dd2cbfad0f611702f9a1f7e
Zishum <you@example.com>
2025-06-06 01:57:52 +0300

    lazy on visible

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,19 @@
-import { lazy } from "react";
 import { useDocumentTitle } from "../functions/useDocumentTitle";
 
 import Slider from "../components/HomeComponents/Slider";
 import LazyOnVisible from "../components/LazyOnVisible";
 import FallBack from "./FallBack";
 
-const HomeNav = lazy(() => import("../components/HomeComponents/HomeNav"));
-
 const Home = () => {
 	useDocumentTitle("ОИК | Официальный сайт | ОИК");
 	return (
 		<>
 			<Slider />
-			<HomeNav />
+			<LazyOnVisible
+				loader={() => import("../components/HomeComponents/HomeNav")}
+				fallback={<FallBack className={"container"} />}
+				rootMargin={"100px"}
+			/>
 			<LazyOnVisible
 				loader={() => import("../components/HomeComponents/Section")}
 				fallback={<FallBack className={"container"} />}
