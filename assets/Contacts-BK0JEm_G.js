fe31bae07b04e711ddb1e31f826217afc6dccb21
Zishum <you@example.com>
2025-06-07 01:21:48 +0300

    Updates

--- assets/Contacts-BK0JEm_G.js
+++
@@ -1,7 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/index-BagV67W4.js","assets/index-CoSvySXp.js","assets/index-ZDeBpZgY.css","assets/index-BqSwFi6U.js","assets/LazyLoadImage-CbJPoSue.js","assets/index-yoe_WagJ.css"])))=>i.map(i=>d[i]);
-import{j as t,r as a,_ as c}from"./index-CoSvySXp.js";import{u as e}from"./useDocumentTitle-BlpaZIBa.js";import{F as r}from"./FallBack-DrzSQnVS.js";const l="_contacts_qj3q6_1",_="_contacts__fallback_qj3q6_15",s={contacts:l,contacts__fallback:_},i="/React/pdf/personal-data-processing-policy.pdf",p=[[{headingBig:"Контакты",text:`Общество с ограниченной ответственностью «Отраслевая инфраструктурная компания (сокращенное наименование — ООО «ОИК»)
-
-ИНН 2130227856
-
-основной код ОКВЭД — 62.09 Деятельность, связанная с использованием вычислительной техники и информационных технологий, прочая
-`},{pdfLink:i,pdfText:"Политика в отношении обработки персональных данных"}]],d=a.lazy(()=>c(()=>import("./index-BagV67W4.js"),__vite__mapDeps([0,1,2]))),m=a.lazy(()=>c(()=>import("./index-BqSwFi6U.js"),__vite__mapDeps([3,1,2,4,5]))),x=()=>t.jsx(t.Fragment,{children:t.jsx("div",{className:`${s.contacts} container`,children:t.jsxs(a.Suspense,{fallback:t.jsx(r,{className:s.contacts__fallback}),children:[p.map((n,o)=>t.jsx(m,{item:n,type:"contacts"},o)),t.jsx(d,{})]})})}),k=()=>(e("Контакты | ОИК"),t.jsx(t.Fragment,{children:t.jsx("div",{className:"margin-top",children:t.jsx(x,{})})}));export{k as default};
--- assets/index-BagV67W4.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-CoSvySXp.js";const e=()=>r.jsx("iframe",{src:"https://yandex.ru/map-widget/v1/?lang=ru_RU&scroll=true&source=constructor-api&um=constructor%3Ae6654a5470774adb8df65f9ba6729011e4d4aa6cf4f2cfb7923c132b0a22be39",allowFullScreen:!0,width:"100%",height:"710px",style:{display:"block",border:0}});export{e as default};
